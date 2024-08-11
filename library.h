e519c3e6406f5459821a0794499a6d66466f2c3c
sylvie <127357510+assembly-seal@users.noreply.github.com>
2024-08-11 14:59:11 -0500

    Update library.h

--- library.h
+++ library.h
@@ -1,5 +1,5 @@
 #if defined(_WIN32) || defined(_WIN64)
-#pragma message( "try again, wincuck: https://ubuntu.com/download/desktop" )
+#pragma message( "try again, wincuck: https://ubuntu.com/tutorials/install-ubuntu-desktop" )
 #else
   // program
 #endif
